3f4725aa0808c0610ece1d526f0bc45b1b86b931
MadhuriShres <madhurishres@gmail.com>
2020-06-27 15:12:42 +0545

    Add events controllers & service objects for Events  list APIs

---
+++ service/app/controllers/events_controller.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+class ApplicationController < ActionController::API
+
+  def index
+    render json: list_events, status: 200
+  end
+
+  def user_events
+    response = current_user.admin? ? list_events : list_member_events
+    render json: response, status: 200
+  end
+
+  private
+
+  def list_events
+    Events.where(list_params)
+  end
+
+  def list_params
+    params.slice(:type, :status, :start_datetime, :end_datetime, :category, :user_id, :mentor_id)
+  end
+
+  def list_member_events
+    return events_template unless current_user.member?
+    { participated_events: ListParticipatedEvents.new(params: user_events_params,
+                                                      current_user: current_user)
+                                                 .call,
+      mentored_events: ListMentoredEvents.new(params: user_events_params,
+                                              current_user: current_user)
+                                         .call }
+  end
+
+  def events_template
+    { participated_events: [], mentored_events: [] }
+  end
+
+  def user_events_params
+    params.slice(:type, :status, :start_datetime, :end_datetime, :category, :tags)
+  end
+end
