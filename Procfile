a9583e98002f8c9249fe7e59178f9103175d0b74
Nirdosh <nrdshgtm@gmail.com>
2020-06-27 20:14:09 +0545

    Add service status endpoint.

--- Procfile
+++
@@ -1,2 +0,0 @@
-web: cd app && yarn start
-api: cd service && bundle exec rails s -p4000
--- service/config/routes.rb
+++ service/config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  get 'service_status/', to: 'service_status#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
