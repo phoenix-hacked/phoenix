ebd9395ca544c8f968abfa013508c0191ed287af
MadhuriShres <madhurishres@gmail.com>
2020-06-27 15:11:54 +0545

    Change:  - Add types & status definitions for user & events  - Add association between event & event_ratings

--- service/app/models/event.rb
+++ service/app/models/event.rb
@@ -1,4 +1,17 @@
 class Event < ApplicationRecord
   belongs_to :user
   has_many :event_participants
+  has_many :event_ratings
+
+  STATUSES = {
+    SCHEDULED => 'scheduled',
+    COMPLETED => 'completed',
+    CANCELLED => 'cancelled',
+    ARCHIVED => 'archived'
+  }
+
+  SCHEDULED = 0
+  COMPLETED = 1
+  CANCELLED = 2
+  ARCHIVED = 3
 end
--- service/app/models/user.rb
+++ service/app/models/user.rb
@@ -3,4 +3,20 @@ class User < ApplicationRecord
   has_many :events
   has_many :event_participants
   has_many :event_ratings
+
+  TYPES = {
+    ADMIN => 'admin',
+    MEMBER => 'member'
+  }
+
+  ADMIN = 0
+  MEMBER = 1
+
+  def admin?
+    type == ADMIN
+  end
+
+  def member?
+    type == MEMBER
+  end
 end
